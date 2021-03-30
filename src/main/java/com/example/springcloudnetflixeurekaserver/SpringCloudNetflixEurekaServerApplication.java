d7414d0c08ceaaa8121ca9935f07f5bc5952183f
lioboal <lioboal@gmail.com>
2021-03-31 00:06:21 +0900

    Second Commit

---
+++ src/main/java/com/example/springcloudnetflixeurekaserver/SpringCloudNetflixEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudnetflixeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudNetflixEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudNetflixEurekaServerApplication.class, args);
+	}
+
+}
