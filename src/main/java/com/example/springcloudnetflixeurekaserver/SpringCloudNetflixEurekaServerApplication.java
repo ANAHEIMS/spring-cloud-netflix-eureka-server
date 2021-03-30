7d2d914a88d7c8d8f26b1965c30b1970d894d6db
lioboal <lioboal@gmail.com>
2021-03-31 01:13:10 +0900

    유레카 서버 구동

--- src/main/java/com/example/springcloudnetflixeurekaserver/SpringCloudNetflixEurekaServerApplication.java
+++ src/main/java/com/example/springcloudnetflixeurekaserver/SpringCloudNetflixEurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.example.springcloudnetflixeurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer /* 유레카 서버 선언 어노테이션 */
 public class SpringCloudNetflixEurekaServerApplication {
 
 	public static void main(String[] args) {
